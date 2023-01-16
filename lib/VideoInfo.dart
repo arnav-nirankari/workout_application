7c5287dbea99de2a2d7848f3d2825c979221559c
arnav-nirankari <arnav.nirankari@student.ufv.ca>
2023-01-15 19:23:09 -0800

    Minor changes to Home Screen

---
+++ lib/VideoInfo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class VideoInfo extends StatefulWidget {
+  const VideoInfo({super.key});
+
+  @override
+  State<VideoInfo> createState() => _VideoInfoState();
+}
+
+class _VideoInfoState extends State<VideoInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
